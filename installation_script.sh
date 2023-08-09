e79eb6a13c5f88ca598b74b800a841edb81e8bf2
Agustine Leo <agustine.leo@smartretail.co>
2023-08-09 11:58:42 +0800

    compress file to prevent aws:downloadContent rate limit calling the api

--- installation_script.sh
+++ installation_script.sh
@@ -1,6 +1,7 @@
 "#!/bin/bash",
 "echo \"Running command in `pwd`\"",
+"echo Untar key_rotate.tar"
+"tar xvf key_rotate.tar"
 "echo \"Run key-rotate.py script.\"",
-"cd ./key-rotate"
 "python3 key-rotate.py \"{{Version}}\""
 "echo \"Done\"",
